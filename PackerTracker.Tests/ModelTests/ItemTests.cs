1af6b4f99fe810c85f297935be05b1ebd224b984
TrevorH08 <trevorh8700@gmail.com>
2022-09-28 16:34:16 -0700

    add all files
    Co-authored-by: Skylan Lew <skylanslew@gmail.com>
    Co-authored-by: Keisha Hepner <kmhepner90@gmail.com>

---
+++ PackerTracker.Tests/ModelTests/ItemTests.cs
@@ -0,0 +1,13 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using PackerTracker.Models;
+
+namespace PackerTracker.Tests
+{
+  [TestClass]
+  public class ItemTests : IDisposable
+  {
+
+  }
+}
